bb8a24ea5959d848f850d692e899b6102aead860
Deepak Awari <dma234@cornell.edu>
2015-03-19 23:29:16 -0400

    Updating Tokenizer and adding new RegEx file

---
+++ src/text/RegEx.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package text;
+
+/**
+ * @author Deepak Awari
+ * @Description Consists of all regular expressions used for NLP package.
+ *
+ */
+public class RegEx 
+{
+	// Regular expression to tokenize paragraphs into sentences
+	public final static String SENTENCETOKENIZER = "(?i)(?<=[.?!])\\S+(?=[a-zA-Z])";
+	
+	// Regular expression to tokenize sentence into words.
+	public final static String WORDTOKENIZER = "";
+	
+	// International Phone numbers
+	
+	
+}
--- src/text/Tokenizer.java
+++ src/text/Tokenizer.java
@@ -4,9 +4,33 @@
 package text;
 
 /**
- * @author Deepak
+ * @author Deepak Awari
  *
  */
-public class Tokenizer {
+public class Tokenizer 
+{
+	/**
+	 * @author Deepak Awari
+	 * @param paragraph - Paragraph to be tokenized into sentences.
+	 * @return String[] - Returns array of sentences
+	 */
+	public String[] tokenizeParagraph(String paragraph)
+	{
+		if((paragraph != null) && (!paragraph.isEmpty()))
+			return paragraph.split(RegEx.SENTENCETOKENIZER);
+		else return null;
+	}
+	
+	/**
+	 * @author Deepak Awari
+	 * @param sentence - Sentence to be tokenized into words.
+	 * @return String[] - Returns array of words
+	 */
+	public String[] tokenizeSentence(String sentence) 
+	{
+		if((sentence != null) && (!sentence.isEmpty()))
+			return sentence.split(RegEx.WORDTOKENIZER);
+		else return null;
+	}
 
 }
